8f0962394668f755bdb872676057a2fc323f9305
Igor Amon <48227869+amonigor@users.noreply.github.com>
2024-07-08 10:33:46 -0300

    add shadcn library

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,30 +1,80 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
-const config: Config = {
+const config = {
+  darkMode: ["class"],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
+  prefix: "",
   theme: {
-    colors: {
-      "bcolor-1": "rgb(var(--bcolor-1) / <alpha-value>)",
-      "bcolor-2": "rgb(var(--bcolor-2) / <alpha-value>)",
-      "interactive-1": "rgb(var(--interactive-1) / <alpha-value>)",
-      "interactive-2": "rgb(var(--interactive-2) / <alpha-value>)",
-      "interactive-3": "rgb(var(--interactive-3) / <alpha-value>)",
-      "border-1": "rgb(var(--border-1) / <alpha-value>)",
-      "border-2": "rgb(var(--border-2) / <alpha-value>)",
-      "border-3": "rgb(var(--border-3) / <alpha-value>)",
-      "primary-1": "rgb(var(--primary-1) / <alpha-value>)",
-      "primary-2": "rgb(var(--primary-2) / <alpha-value>)",
-      "tcolor-1": "rgb(var(--tcolor-1) / <alpha-value>)",
-      "tcolor-2": "rgb(var(--tcolor-2) / <alpha-value>)",
-      muted: "rgb(var(--muted) / <alpha-value>)",
-      danger: "rgb(var(--danger) / <alpha-value>)",
-      success: "rgb(var(--success) / <alpha-value>)",
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
-  plugins: [],
-};
-export default config;
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
+
+export default config
